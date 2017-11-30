19ee639556ac1e27e97d94a0abb0dc93e5a91fa0
DANIELGG98 <dagonzalez9804@gmail.com>
2017-11-30 01:02:55 -0500

    Add files via upload

---
+++ Multiplicacion_Matrices/src/multiplicacion_matrices/Matriz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion_matrices;
+
+/**
+ *
+ * @author Equipo01
+ */
+public class Matriz {
+    
+    int Num_filas;
+    int Num_columnas;
+    
+}
